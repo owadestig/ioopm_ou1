84f078fc36dc0be6051d7168206f80a591ca257a
Oscar Wadestig <oscar@thewades.se>
2020-10-29 16:21:46 +0100

    stuff

---
+++ hash_table.h
@@ -0,0 +1,42 @@
+
+typedef struct entry entry_t;
+typedef struct hash_table ioopm_hash_table_t;
+
+/**
+ * @file hash_table.h
+ * @author Oscar Wadestig
+ * @date 29/10/2020
+ * @brief Simple hash table that maps integer keys to string values.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ *
+ * @see http://wrigstad.com/ioopm/assignments/assignment1.html
+ */
+
+/// @brief Create a new hash table
+/// @return A new empty hash table
+ioopm_hash_table_t *ioopm_hash_table_create();
+
+/// @brief Delete a hash table and free its memory
+/// param ht a hash table to be deleted
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht);
+
+/// @brief add key => value entry in hash table ht
+/// @param ht hash table operated upon
+/// @param key key to insert
+/// @param value value to insert
+void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value);
+
+/// @brief lookup value for key in hash table ht
+/// @param ht hash table operated upon
+/// @param key key to lookup
+/// @return the value mapped to by key (FIXME: incomplete)
+void *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key);
+
+/// @brief remove any mapping from key to a value
+/// @param ht hash table operated upon
+/// @param key key to remove
+/// @return the value mapped to by key (FIXME: incomplete)
+char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key);
