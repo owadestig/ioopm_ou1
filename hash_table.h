fee1e67ac66fee0ca78ac4b9a6e3ffcb3a1e0717
Oscar Wadestig <oscar@thewades.se>
2020-10-30 11:57:10 +0100

    More code

--- hash_table.h
+++ hash_table.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool. h> 
+
 
 typedef struct entry entry_t;
 typedef struct hash_table ioopm_hash_table_t;
@@ -15,28 +19,54 @@ typedef struct hash_table ioopm_hash_table_t;
  * @see http://wrigstad.com/ioopm/assignments/assignment1.html
  */
 
-/// @brief Create a new hash table
-/// @return A new empty hash table
-ioopm_hash_table_t *ioopm_hash_table_create();
+/// @brief returns the number of key => value entries in the hash table
+/// @param h hash table operated upon
+/// @return the number of key => value entries in the hash table
+int ioopm_hash_table_size(ioopm_hash_table_t *h);
 
-/// @brief Delete a hash table and free its memory
-/// param ht a hash table to be deleted
-void ioopm_hash_table_destroy(ioopm_hash_table_t *ht);
+/// @brief checks if the hash table is empty
+/// @param h hash table operated upon
+/// @return true is size == 0, else false
+bool ioopm_hash_table_is_empty(ioopm_hash_table_t *h);
 
-/// @brief add key => value entry in hash table ht
-/// @param ht hash table operated upon
-/// @param key key to insert
-/// @param value value to insert
-void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value);
+/// @brief clear all the entries in a hash table
+/// @param h hash table operated upon
+void ioopm_hash_table_clear(ioopm_hash_table_t *h);
 
-/// @brief lookup value for key in hash table ht
-/// @param ht hash table operated upon
-/// @param key key to lookup
-/// @return the value mapped to by key (FIXME: incomplete)
-void *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key);
+/// @brief return the keys for all entries in a hash map (in no particular order, but same as ioopm_hash_table_values)
+/// @param h hash table operated upon
+/// @return an array of keys for hash table h
+int *ioopm_hash_table_keys(ioopm_hash_table_t *h);
 
-/// @brief remove any mapping from key to a value
-/// @param ht hash table operated upon
-/// @param key key to remove
-/// @return the value mapped to by key (FIXME: incomplete)
-char *ioopm_hash_table_remove(ioopm_hash_table_t *ht, int key);
+/// @brief return the values for all entries in a hash map (in no particular order, but same as ioopm_hash_table_keys)
+/// @param h hash table operated upon
+/// @return an array of values for hash table h
+char **ioopm_hash_table_values(ioopm_hash_table_t *h);
+
+/// @brief check if a hash table has an entry with a given key
+/// @param h hash table operated upon
+/// @param key the key sought
+bool ioopm_hash_table_has_key(ioopm_hash_table_t *h, int key);
+
+/// @brief check if a hash table has an entry with a given value
+/// @param h hash table operated upon
+/// @param value the value sought
+bool ioopm_hash_table_has_value(ioopm_hash_table_t *h, char *value);
+
+/// @brief check if a predicate is satisfied by all entries in a hash table
+/// @param h hash table operated upon
+/// @param pred the predicate
+/// @param arg extra argument to pred
+bool ioopm_hash_table_all(ioopm_hash_table_t *h, ioopm_apply_function pred, void *arg);
+
+/// @brief check if a predicate is satisfied by any entry in a hash table
+/// @param h hash table operated upon
+/// @param pred the predicate
+/// @param arg extra argument to pred
+bool ioopm_hash_table_any(ioopm_hash_table_t *h, ioopm_apply_function pred, void *arg);
+
+/// @brief apply a function to all entries in a hash table
+/// @param h hash table operated upon
+/// @param apply_fun the function to be applied to all elements
+/// @param arg extra argument to apply_fun
+void ioopm_hash_table_apply_to_all(ioopm_hash_table_t *h, ioopm_apply_function apply_fun, void *arg);
---
+++ hash_test.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_table.h"
+
