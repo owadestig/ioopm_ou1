d087529d816b0e4faa472b06ebac364ac01918c7
Oscar Wadestig <oscar@thewades.se>
2020-11-09 11:31:48 +0100

    fixed small bug

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 int main(int argc, char const *argv[])
 {
     ioopm_hash_table_t *ht = ioopm_hash_table_create();
-    ioopm_hash_table_insert(ht,5,'e');
-    ioopm_hash_table_insert(ht,3,'c');
-    ioopm_hash_table_insert(ht,1,'a');
-    ioopm_hash_table_insert(ht,4,'d');
-    ioopm_hash_table_insert(ht,2,'b');
+    ioopm_hash_table_insert(ht,5,"e");
+    ioopm_hash_table_insert(ht,3,"c");
+    ioopm_hash_table_insert(ht,1,"a");
+    ioopm_hash_table_insert(ht,4,"d");
+    ioopm_hash_table_insert(ht,2,"b");
     ioopm_hash_table_keys(ht);
     ioopm_hash_table_values(ht);
     ioopm_hash_table_remove(ht,6);
