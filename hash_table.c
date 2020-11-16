e445a0a83e2e43d954c829b322d5d2f55e992150
Oscar Wadestig <oscar@thewades.se>
2020-11-16 13:19:15 +0100

    trying work around

--- hash_table.c
+++ hash_table.c
@@ -284,7 +284,9 @@ char **ioopm_hash_table_values(ioopm_hash_table_t *ht)
 {
     int amount = ioopm_hash_table_size(ht);
     int i = 0;
-    char **values = calloc(1, amount * sizeof(char *));
+    char *arr = calloc(1, amount * sizeof(char *));
+    char (*values)[amount];
+    values = &arr; 
 
     for (int j = 0; j < No_Buckets; j++)
     {
@@ -294,18 +296,13 @@ char **ioopm_hash_table_values(ioopm_hash_table_t *ht)
         {
             char *temp = calloc(1, sizeof(entry->value));
             strcpy(temp, entry->value);
-            values[i] = temp;
+            arr[i] = temp;
             entry = entry->next;
             i++;
         }
     }
 
-    for (int i = 0; i < amount; i++)
-    {
-        fprintf(stdout, "Value %d = %s\n", amount, values[i]);
-    }
-
-    return values;
+    return arr;
 }
 
 /// @brief prints all keys in hash table
